c61d22e886ef683aa1187bfa51ff875ef0a347d0
MuhammadDevelop <developermuhammad449@gmail.com>
2026-08-01 08:14:48 +0300

    sdfghjk

---
+++ index/index.js
@@ -0,0 +1,5 @@
+//for loop agar aylanib chiqmoqchi bo'lgan element aniq bo'lmasa ishlaydi 
+//while loop agr aylanib chiqmoqchi bo'lgan element aniq bo'lsa ishlaydi
+//switch case 
+//github git command 
+//vercel && netlify
